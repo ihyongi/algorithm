435cfc0b788d89b1f6fffba1e0a52ddb8f6677c5
ihyongi <ihyongi@github.com>
2022-09-05 22:28:46 +0900

    220905 으아아!!!!

---
+++ src/Chaining.java
@@ -0,0 +1,69 @@
+public class Chaining {
+    public Slot[] hashTable;
+
+    public Chaining(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String key; //체이닝 추가
+        String value;
+        Slot next;
+        Slot(String key, String value) { //체이닝 추가
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) { //내부가 링크드리스트인경우
+            Slot findSlot = this.hashTable[address]; //1.변수두개선언
+            Slot prevSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if (findSlot.key == key) {//내가찾는 키값인지?
+                    findSlot.value = value; //아 찾은 슬롯에 밸류 할당하고 끗
+                    return true;
+                } else {
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
+                }
+            }
+            prevSlot.next = new Slot(key, value); //링크드리스트에없으니 새로 넣어주면된다
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address]; //링크드리스트를 순회하기위함
+            while (findSlot != null) {
+                if (findSlot.key == key) {
+                    return findSlot.value;
+                } else {
+                    findSlot = findSlot.next;
+                }
+            }
+            return null; //링크드리스트안에 없다
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Chaining chaining = new Chaining(20);
+        chaining.saveData("DaveLee", "01022223333");
+        chaining.saveData("fun-coding", "01033334444");
+        chaining.saveData("David", "01044445555");
+        chaining.saveData("Dave", "01055556666");
+        chaining.getData("Dave");
+    }
+}
---
+++ src/MyHash.java
@@ -0,0 +1,57 @@
+public class MyHash {
+    public Slot[] hashTable;
+
+    public MyHash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String value;
+        Slot(String value) {
+            this.value = value;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length; //키를 받아서 해쉬테이블의 개수로 나눠서 나머지가져옴,,사이즈-1까지의 값을 가지게됨
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            return this.hashTable[address].value;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MyHash mainObject = new MyHash(20);
+        mainObject.saveData("DaveLee", "01022223333");
+        mainObject.saveData("fun-coding", "01033334444");
+        mainObject.getData("fun-coding"); //키값으로 가져옴
+
+
+        //--충돌
+        MyHash mainObject2 = new MyHash(20);
+        mainObject2.saveData("DaveLee", "01022223333"); //D
+        mainObject2.saveData("fun-coding", "01033334444");
+        mainObject2.saveData("David", "01044445555"); //D
+        mainObject2.saveData("Dave", "01055556666"); //D
+        mainObject2.getData("DaveLee");//D 충돌..!
+
+
+
+    }
+}
