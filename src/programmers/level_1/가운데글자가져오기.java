fa8ad611b4acb311e17828923b1c2c83f42dfe28
ihyongi <025031210@naver.com>
2022-10-25 21:22:51 +0900

    히히 오늘도 해따 ^_ㅠ

---
+++ src/programmers/level_1/가운데글자가져오기.java
@@ -0,0 +1,20 @@
+package programmers.level_1;
+
+public class 가운데글자가져오기 {
+    /**
+     * 단어 s의 가운데 글자를 반환하는 함수,
+     * solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+     *
+     * s는 길이가 1 이상, 100이하인 스트링입니다.
+     */
+
+    class Solution {
+        public String solution(String s) {
+            String answer = "";
+            int length = s.length();
+            if (length % 2 == 0) answer = s.substring(length/2-1, length/2+1);
+            else answer = s.substring(length/2,(length/2)+1);
+            return answer;
+        }
+    }
+}
