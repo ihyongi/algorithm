2c6b416a2fd76795b6d5b51aedcc19da1991aebe
ihyongi <025031210@naver.com>
2023-07-03 20:15:39 +0900

    임시 커밋

--- src/baekjoon/심화(6)/단어공부_1157.kt
+++ src/baekjoon/심화(6)/단어공부_1157.kt
@@ -10,6 +10,7 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
 /**
  * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
  *
+ * 맵에 담아서 plus 1 할 수 있구나,,
  * Mississipi
  *         m{m=1}
  *         i{m=1, i=1}
