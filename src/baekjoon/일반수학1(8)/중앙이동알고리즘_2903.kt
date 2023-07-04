44c9a5642696a3b9f17c0737e4b12165b58b2b88
he.lee <he.lee@11h11m.com>
2023-07-04 17:25:17 +0900

    임시 커밋

--- src/baekjoon/일반수학1(8)/중앙이동알고리즘_2903.kt
+++ src/baekjoon/일반수학1(8)/중앙이동알고리즘_2903.kt
@@ -23,10 +23,21 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
  *
  *
  * 몬말이야?
+ *
+ * 패턴을 우선 보면 모든 점의 수는 한변의 점의 제곱이다. 점의 갯수는 늘어날때마다 현재 점의 -1만큼 늘어난다.
+ * 처음에 2개였던 점이 다음에 2+(2-1)가 되어서 3이 되고, 다음엔 3+(3-1) = 5, 또 다음엔 5+(5-1) = 9 이렇게 늘어난다.
+ * 그렇다면 반복문으로 입력값만큼 한 변의 점의 갯수를 늘리고 그 갯수를 제곱해서 출력한다.
  */
 
     val w = BufferedWriter(OutputStreamWriter(System.out))
-    w.write("")
+    val n = readLine().toInt()
+    var d = 2
+
+    for (i in 0 until n){
+        d += (d - 1)
+    }
+
+    w.write("${d*d}")
     w.flush()
     w.close()
     close()
