f4ec21d86ada567b049f5cf0486c1bb89bd0139c
이효은 <73655077+ihyongi@users.noreply.github.com>
2023-11-15 23:26:59 +0900

    넘모 어렵다..

--- src/fastcampus/class02/ch02시간복잡도/개미_10158.java
+++ src/fastcampus/class02/ch02시간복잡도/개미_10158.java
@@ -12,24 +12,31 @@ class Main_10158 {
         int Q = sc.nextInt(); //초기위치
         int T = sc.nextInt(); //시간
 
-        int timeX = T % (2 * W);
-        int currentX = P;
-        int deltaX = 1;
-        while (timeX -- >0){
-            if(currentX == W) deltaX =-1;
-            else if(currentX == 0)deltaX =1;
-            currentX += deltaX;
-        }
+        //풀이 1 O(max(W,H))
+//        int timeX = T % (2 * W);
+//        int currentX = P;
+//        int deltaX = 1;
+//        while (timeX -- >0){
+//            if(currentX == W) deltaX =-1;
+//            else if(currentX == 0)deltaX =1;
+//            currentX += deltaX;
+//        }
+//
+//        int timeY = T % (2 * H);
+//        int currentY = Q;
+//        int deltaY =1;
+//        while (timeY -- >0){
+//            if(currentY == H) deltaY =-1;
+//            else if(currentY == 0)deltaY =1;
+//            currentY += deltaY;
+//        }
 
-        int timeY = T % (2 * H);
-        int currentY = Q;
-        int deltaY =1;
-        while (timeY -- >0){
-            if(currentY == H) deltaY =-1;
-            else if(currentY == 0)deltaY =1;
-            currentY += deltaY;
-        }
-        //오잉
+        //풀이 2 O(1)
+        int currentX = (T + P) % (2 * W);
+        int currentY = (T + Q) % (2 * H);
+        if (currentX > W) currentX = 2 * W - currentX;
+        if (currentY > H) currentY = 2 * H - currentY;
+
         System.out.println(currentX + " " + currentY);
 
 
