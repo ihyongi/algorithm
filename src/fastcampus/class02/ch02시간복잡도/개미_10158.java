32dc1f8d74a3ab3a39877e57374bf8762670d4ec
이효은 <73655077+ihyongi@users.noreply.github.com>
2023-12-05 22:52:43 +0900

    타임아웃나는거 다시

--- src/fastcampus/class02/ch02시간복잡도/개미_10158.java
+++ src/fastcampus/class02/ch02시간복잡도/개미_10158.java
@@ -1,16 +1,17 @@
 package fastcampus.class02.ch02시간복잡도;
 
+import java.io.*;
 import java.util.Scanner;
 
 class Main_10158 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //next()는 공백을 만나면 멈춘다. nextLine()는 공백포함
-        int W = sc.nextInt(); //크기1
-        int H = sc.nextInt(); //크기2
-        int P = sc.nextInt(); //초기위치
-        int Q = sc.nextInt(); //초기위치
-        int T = sc.nextInt(); //시간
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        //next()는 공백을 만나면 멈춘다. nextLine()는 공백포함
+//        int W = sc.nextInt(); //크기1
+//        int H = sc.nextInt(); //크기2
+//        int P = sc.nextInt(); //초기위치
+//        int Q = sc.nextInt(); //초기위치
+//        int T = sc.nextInt(); //시간
 
         //풀이 1 O(max(W,H))
 //        int timeX = T % (2 * W);
@@ -32,13 +33,36 @@ class Main_10158 {
 //        }
 
         //풀이 2 O(1)
+//        int currentX = (T + P) % (2 * W);
+//        int currentY = (T + Q) % (2 * H);
+//        if (currentX > W) currentX = 2 * W - currentX;
+//        if (currentY > H) currentY = 2 * H - currentY;
+//
+//        System.out.println(currentX + " " + currentY);
+//    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력
+        int W = Integer.parseInt(br.readLine());
+        int H = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+        int Q = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+
+        // 계산
         int currentX = (T + P) % (2 * W);
         int currentY = (T + Q) % (2 * H);
         if (currentX > W) currentX = 2 * W - currentX;
         if (currentY > H) currentY = 2 * H - currentY;
 
-        System.out.println(currentX + " " + currentY);
+        // 출력
+        bw.write(currentX + " " + currentY + "\n");
 
-
+        // 닫기
+        br.close();
+        bw.close();
     }
 }
